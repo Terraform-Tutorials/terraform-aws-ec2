9ba8628c08c7b7029df3a347523f674795715d3d
amaurybsouza <amaurybsouza@gmail.com>
2021-09-13 22:01:52 -0300

    new files .tf to provision resources

---
+++ provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_instance" "tutorials" {
+  ami           = var.ami
+  instance_type = var.instance_type
+}
+
---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+  description = "define what region"
+  default     = "us-east-1"
+}
+
+variable "ami" {
+  description = "define what AMI"
+  default     = "ami-09e67e426f25ce0d7"
+}
+
+variable "instance_type" {
+  description = "value"
+  default     = "t2.micro"
+}     
